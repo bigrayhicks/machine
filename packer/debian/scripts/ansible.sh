e02583436d81c018598d7b52cc234a1405f3f665
Stig Lindqvist <stojg.lindqvist@gmail.com>
2016-07-06 17:47:21 +1200

    install ansible via pip

--- packer/debian/scripts/ansible.sh
+++ packer/debian/scripts/ansible.sh
@@ -3,6 +3,12 @@
 # Ansible prequesites
 #apt-get -y install python ruby ruby-json facter ohai
 
+
+apt-get install -y python-setuptools
+easy_install pip
+pip install paramiko PyYAML Jinja2 httplib2 six
+pip install ansible
+
 # Install backports for wheezy
 if [ $(facter lsbdistcodename) = 'wheezy' ] ; then
     echo "deb http://http.debian.net/debian wheezy-backports main" > /etc/apt/sources.list.d/backports.list
